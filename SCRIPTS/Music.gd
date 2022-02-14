b29a036fa70b766450108faa85bbcc86f1c6b4af
DomestiqueBaston <ferdi2x@gmail.com>
2022-02-14 17:42:41 +0100

    Update... Mainly focused on having music play right in the background!

---
+++ SCRIPTS/Music.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var music_clip_start : AudioStream = load("res://ASSETS/SOUNDS/MUSIC/START.wav")
+var music_clip_loop : AudioStream = load("res://ASSETS/SOUNDS/MUSIC/LOOP.wav")
+
+func _ready():
+	whole_music()
+
+func whole_music():
+	play(music_clip_start)
+	yield($Bernard, "finished")
+	play(music_clip_loop)
+
+func play(clip: AudioStream):
+	$Bernard.stream = clip
+	$Bernard.play()
+
+
--- SCRIPTS/TEST.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-var start_music = load("res://ASSETS/SOUNDS/MUSIC/In game music_START.wav")
-var loop_music = load("res://ASSETS/SOUNDS/MUSIC/In game music_LOOP.wav")
-
