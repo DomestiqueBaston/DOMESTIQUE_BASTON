764e1ad7f7d924158630fe904ce91ff53b71f8c2
DomestiqueBaston <ferdi2x@gmail.com>
2022-06-27 09:27:27 +0200

    Rewinding...

--- SCRIPTS/Managers/CharacterSelectionManager.gd
+++ SCRIPTS/Managers/CharacterSelectionManager.gd
@@ -3,5 +3,5 @@ extends Node
 var player
 var opponent
 
-var playerScript = preload("res://SCRIPTS/Characters/Moulue_05.gd")
-var aiScript = preload("res://SCRIPTS/Characters/Couillu_05.gd")
+var playerScript = preload("res://SCRIPTS/Characters/Moulue.gd")
+var aiScript = preload("res://SCRIPTS/Characters/Couillu.gd")
