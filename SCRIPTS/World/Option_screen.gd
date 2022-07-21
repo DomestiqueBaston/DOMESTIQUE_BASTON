0ff2c2a347753f1a155003b105e23d37eeab065c
DOMESTIQUE BASTON <97660855+DomestiqueBaston@users.noreply.github.com>
2022-07-21 15:00:18 +0200

    Optimized Start_screen and working on Option_screen...

---
+++ SCRIPTS/World/Option_screen.gd
@@ -0,0 +1,75 @@
+extends Control
+
+
+
+var previous_vpos := 0
+var current_vpos := 0
+var previous_hpos := 0
+var current_hpos := 0
+var current_node := "0_0"
+var previous_node := ""
+
+func _ready():
+	pass
+
+func _physics_process(_delta):
+	if (Input.is_action_just_pressed("ui_down")):
+		previous_vpos = current_vpos
+		check_previous_boundaries()
+		current_vpos += 1
+		check_current_boundaries()
+		recreate_node_names()
+		turn_on_off()
+
+	elif (Input.is_action_just_pressed("ui_up")):
+		previous_vpos = current_vpos
+		check_previous_boundaries()
+		current_vpos -= 1
+		check_current_boundaries()
+		recreate_node_names()
+		turn_on_off()
+
+	if (Input.is_action_just_pressed("ui_right")):
+		previous_hpos = current_hpos
+		check_previous_boundaries()
+		current_hpos += 1
+		check_current_boundaries()
+		recreate_node_names()
+		turn_on_off()
+
+	elif (Input.is_action_just_pressed("ui_left")):
+		previous_hpos = current_hpos
+		check_previous_boundaries()
+		current_hpos -= 1
+		check_current_boundaries()
+		recreate_node_names()
+		turn_on_off()
+
+func check_previous_boundaries():
+	if previous_vpos < 0:
+		previous_vpos = 2
+	elif previous_vpos > 2:
+		previous_vpos = 0
+	elif previous_hpos < 0:
+		previous_hpos = 3
+	elif previous_hpos > 3:
+		previous_hpos = 0
+
+func check_current_boundaries():
+	if current_vpos < 0:
+		current_vpos = 2
+	elif current_vpos > 2:
+		current_vpos = 0
+	elif current_hpos < 0:
+		current_hpos = 3
+	elif current_hpos > 3:
+		current_hpos = 0
+		
+func recreate_node_names():
+	previous_node = str(previous_vpos) + "_" + str(previous_hpos)
+	current_node = str(current_vpos) + "_" + str(current_hpos)
+
+func turn_on_off():
+	print (str(previous_node) + " - " + str(current_node))
+#	get_node(previous_node).modulate = Color(1,1,1,0)
+#	get_node(current_node).modulate = Color(1,1,1,1)
--- SCRIPTS/World/Start_screen.gd
+++ SCRIPTS/World/Start_screen.gd
@@ -15,8 +15,8 @@ func _physics_process(_delta):
 		check_previous_boundaries()
 		current_pos += 1
 		check_current_boundaries()
-		previous_pos_to_previous_node()
-		current_pos_to_current_node()
+		previous_node = str(previous_pos)
+		current_node = str(current_pos)
 		turn_on_off()
 
 	elif (Input.is_action_just_pressed("ui_up")):
@@ -24,10 +24,9 @@ func _physics_process(_delta):
 		check_previous_boundaries()
 		current_pos -= 1
 		check_current_boundaries()
-		previous_pos_to_previous_node()
-		current_pos_to_current_node()
+		previous_node = str(previous_pos)
+		current_node = str(current_pos)
 		turn_on_off()
-
 
 func check_current_boundaries():
 	if current_pos < 0:
@@ -41,30 +40,8 @@ func check_previous_boundaries():
 	elif previous_pos > 3:
 		previous_pos = 0
 
-func current_pos_to_current_node():
-	if current_pos == 0:
-		current_node = "Reglons_ON"
-	elif current_pos == 1:
-		current_node = "Changer_ON"
-	elif current_pos == 2:
-		current_node = "Pondu_ON"
-	else:
-		current_node = "Decarrer_ON"
-
-
-func previous_pos_to_previous_node():
-	if previous_pos == 0:
-		previous_node = "Reglons_ON"
-	elif previous_pos == 1:
-		previous_node = "Changer_ON"
-	elif previous_pos == 2:
-		previous_node = "Pondu_ON"
-	else:
-		previous_node = "Decarrer_ON"
-
 func turn_on_off():
 	get_node(previous_node).modulate = Color(1,1,1,0)
 	get_node(current_node).modulate = Color(1,1,1,1)
-#	get_node(previous_node).hide()
-#	get_node(current_node).show()
+
 
