38b8b2c3327feab7d452d8b7570ee9ece43e8fcb
Geoff Levner <glevner@gmail.com>
2022-07-26 22:16:01 +0200

    Cleaned up code in Moulue.gd
    But it should behave just the same, for now...

--- SCRIPTS/Characters/Moulue.gd
+++ SCRIPTS/Characters/Moulue.gd
@@ -1,68 +1,58 @@
 extends KinematicBody2D
 
-
 const SPEED = 50
 const FLOOR = Vector2.UP #Vector2(0, -1)
 
 var velocity = Vector2()
-
-var isDoingSomething = false
-
+var busy = false
 
 func _physics_process(_delta):
-	if Input.is_action_pressed("ui_right") && isDoingSomething == false:
+	if busy:
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED * 1.5
 		$AnimatedSprite.play("Walk_forward")
-	elif Input.is_action_pressed("ui_left") && isDoingSomething == false:
+	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
 		$AnimatedSprite.play("Walk_backward")
 	else:
 		velocity.x = 0
-		if isDoingSomething == false:
-			$AnimatedSprite.play("Idle")
-			
-	if Input.is_action_pressed("top_left") && Input.is_action_pressed("top_right") && isDoingSomething == false:
-		$AnimatedSprite.play("Parry")
-		isDoingSomething = true
-		
-	elif Input.is_action_just_pressed("top_left") && isDoingSomething == false:
-		$AnimatedSprite.play("Crouch")
-		isDoingSomething = true
-		
-	elif Input.is_action_just_pressed("top_right") && isDoingSomething == false:
-		$AnimatedSprite.play("Jump")
-		isDoingSomething = true
+		$AnimatedSprite.play("Idle")
 
-	elif Input.is_action_pressed("ui_a") && Input.is_action_pressed("ui_b") && isDoingSomething == false:
-		$AnimatedSprite.play("Slap")
-		isDoingSomething = true
+	if !busy:
+		if Input.is_action_pressed("top_left") && Input.is_action_pressed("top_right"):
+			$AnimatedSprite.play("Parry")
+			busy = true
+		elif Input.is_action_just_pressed("top_left"):
+			$AnimatedSprite.play("Crouch")
+			busy = true
+		elif Input.is_action_just_pressed("top_right"):
+			$AnimatedSprite.play("Jump")
+			busy = true
+		elif Input.is_action_pressed("ui_a") && Input.is_action_pressed("ui_b"):
+			$AnimatedSprite.play("Slap")
+			busy = true
+		elif Input.is_action_just_pressed("ui_a"):
+			$AnimatedSprite.play("Punch")
+			busy = true
+		elif Input.is_action_just_pressed("ui_b"):
+			$AnimatedSprite.play("Kick")
+			busy = true
+		elif Input.is_action_pressed("ui_x") && Input.is_action_pressed("ui_y"):
+			$AnimatedSprite.play("Slash")
+			busy = true
+		elif Input.is_action_just_pressed("ui_x"):
+			$AnimatedSprite.play("Insult")
+			busy = true
+		elif Input.is_action_just_pressed("ui_y"):
+			$AnimatedSprite.play("Trick")
+			busy = true
 
-	elif Input.is_action_just_pressed("ui_a") && isDoingSomething == false:
-		$AnimatedSprite.play("Punch")
-		isDoingSomething = true
-
-	elif Input.is_action_just_pressed("ui_b") && isDoingSomething == false:
-		$AnimatedSprite.play("Kick")
-		isDoingSomething = true
-
-	elif Input.is_action_pressed("ui_x") && Input.is_action_pressed("ui_y") && isDoingSomething == false:
-		$AnimatedSprite.play("Slash")
-		isDoingSomething = true
-
-	elif Input.is_action_just_pressed("ui_x") && isDoingSomething == false:
-		$AnimatedSprite.play("Insult")
-		isDoingSomething = true
-
-	elif Input.is_action_just_pressed("ui_y") && isDoingSomething == false:
-		$AnimatedSprite.play("Trick")
-		isDoingSomething = true
-		
 	velocity = move_and_slide(velocity, FLOOR)
-
 
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation != "Walk_forward" and $AnimatedSprite.animation != "Walk_backward" and $AnimatedSprite.animation != "Idle":
-		isDoingSomething = false
+		busy = false
 		
 		
 		
